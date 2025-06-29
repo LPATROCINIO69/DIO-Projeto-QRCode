40cae8edebf6e204487d25205cfb01e01aae215e
LPATROCINIO69 <luciopp69@gmail.com>
2025-06-29 11:41:47 -0300

    First commit

---
+++ src/prompts/prompt-main.js
@@ -0,0 +1,13 @@
+import chalk from "chalk";
+
+const mainPrompt = [
+    {
+        name:"select",
+        description: chalk.bgYellow.blue.bold("Escolha a ferramenta (1 - QRCODE ou (2 - PASSWORD"),
+        pattern: /^[1-2]+$/,
+        message: chalk.red.italic("Escolha apenas entre 1 ou 2"),
+        required: true
+    }
+];
+
+export default mainPrompt;
