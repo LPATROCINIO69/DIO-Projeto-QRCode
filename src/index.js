2126f7a2529f8ca919d8c279127efb03b75aa6b9
LPATROCINIO69 <luciopp69@gmail.com>
2025-06-29 15:31:43 -0300

    Incluindo versão de geração de QRCODE

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import prompt from 'prompt';
 import mainPrompt from './prompts/prompt-main.js';
 import chalk from 'chalk';
-
+import createQRCode from './services/qr-code/create.js';
+import createPassword from './services/password/create.js';
 
 async function main(){
     prompt.get(mainPrompt, async (err,choose)=>{
-        if (choose.select == 1) console.log(chalk.blue("Escolheu o QRCode"));
-        if (choose.select == 2) console.log(chalk.blue("Escolheu o PASSWORD"));
+        if (choose.select == 1) {
+            console.log(chalk.blue("Escolheu o QRCode"));
+            await createQRCode();
+        }
+        if (choose.select == 2) {
+            console.log(chalk.blue("Escolheu o PASSWORD"));
+            await createPassword();
+        }
     })
 }
 
---
+++ src/services/password/create.js
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+
+async function createPassword() {
+    console.log(chalk.green("pPassword"));
+}
+
+export default createPassword;
